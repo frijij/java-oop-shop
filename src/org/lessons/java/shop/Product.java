713cb7c090b88481a9fe8e81e0e5a35ab267e766
francesca <francipeco36@gmail.com>
2023-07-20 14:07:33 +0200

    file preparation

---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,42 @@
+package org.lessons.java.shop;
+
+public class Product {
+    /*
+    Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- descrizione
+- prezzo
+- iva
+
+fare in modo che:
+- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+- Il codice prodotto sia accessibile solo in lettura
+- Gli altri attributi siano accessibili sia in lettura che in scrittura
+- Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+- Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome: il codice deve avere un pad left di zeri per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+     */
+
+    // CAMPI: codice (numero intero), nome, descrizione, prezzo, iva
+
+
+
+    // COSTRUTTORI
+
+
+
+    //GETTERS E SETTERS
+// getter solo codice prodotto
+    // getter + setter per tutti gli altri
+
+
+    // METODI
+    // 1. metodo per crazione random codice prodotto
+
+    //2. prezzo base
+
+    //3. prezzo con iva
+
+    //4. concatenazione nome e codice prodotto
+
+}
